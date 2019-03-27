d35ef1cdad6199723d1626e3e0518b2a419eaf71
Luis Alejandro <isaaca.marquez@alumnos.udg.mx>
2019-03-27 14:28:53 -0600

    Segunda parte IONIC agregamos segunda pagina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { InfoPage } from '../info/info';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
   casas = [];
+  infoPage = InfoPage;
 
   constructor(public navCtrl: NavController, public http: HttpClient) {
     this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15')
@@ -17,6 +19,11 @@ export class HomePage {
     }, error => {
       console.log(JSON.stringify(error));
     });
+  }
+
+  info (casa){
+    console.log('click');
+    this.navCtrl.push(this.infoPage, {casa: casa});
   }
 
 }
